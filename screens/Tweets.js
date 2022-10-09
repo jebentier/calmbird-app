3ef965f756fe075929eb8f16161176f8d72c9913
James Ebentier <jebentier@gmail.com>
2022-10-09 17:02:21 +0200

    Fill in proper content for my tweets page

--- screens/Tweets.js
+++ screens/Tweets.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useSelector } from 'react-redux'
 import {
   Text,
   View,
+  FlatList,
+  RefreshControl,
 } from 'react-native';
+import { authedFetch } from '../shared/api';
+import Tweet from '../shared/tweet';
 import styles from '../shared/styles';
 
-export default function Tweets() {
+export default () => {
   const user = useSelector((state) => state.auth.user)
+
+  const [content, setContent] = useState();
+  const [loading, setLoading] = useState();
+  const [error, setError] = useState();
+
+  const loadTimeline = () => {
+    authedFetch('timeline', { token: user.token, setError, setLoading }).then(setContent);
+  };
+
+  const onRefresh = useCallback(loadTimeline);
+
+  useEffect(loadTimeline, [user]);
 
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={styles.title}>Hi {user.username}!</Text>
-      </View>
+      {error !== undefined && <Text style={styles.error}>Error: {error}</Text>}
+      {content !== undefined &&
+        <FlatList
+          contentContainerStyle={{ paddingBottom: 25 }}
+          data={ content.tweets }
+          renderItem={({ item }) => (<Tweet {...item} currentUser={user} />)}
+          refreshControl={
+            <RefreshControl refreshing={loading} {...{onRefresh }} />
+          }
+        />
+      }
     </View>
   );
-}
+};
